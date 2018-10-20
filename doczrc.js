fc54977258da52484afef5419a142b4553e9ec3a
Rafael Bernardo <rafamaxber@gmail.com>
2018-10-19 21:54:40 -0300

    Add docz

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Spotifood',
+  description: 'This is Spotifood component documentation',
+  themeConfig: {
+    colors: {
+      primary: '#7f060a',
+      secondary: 'khaki',
+      gray: 'lightslategray'
+    }
+  },
+  dest: '/dest-docz',
+  port: '3001'
+}
