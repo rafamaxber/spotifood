58e20c1394931acdec472ae96416fb23bc85870f
Rafael Bernardo <rafamaxber@gmail.com>
2018-10-21 20:07:56 -0300

    wip

--- src/containers/Filters/FeaturedPlaylist.jsx
+++ src/containers/Filters/FeaturedPlaylist.jsx
@@ -15,103 +15,10 @@ import { Loading } from '../../components/Loading/index'
 
 import * as storeActions from '../../store/featuredPlaylist'
 
-/*
-const mock = {
-  filters: [
-    {
-      id: 'locale',
-      name: 'Locale',
-      values: [
-        {
-          value: 'en_AU',
-          name: 'en_AU'
-        },
-        {
-          value: 'de_DE',
-          name: 'de_DE '
-        },
-        {
-          value: 'pt_BR',
-          name: 'pt_BR'
-        },
-        {
-          value: 'fr_FR',
-          name: 'fr_FR'
-        },
-        {
-          value: 'en_US',
-          name: 'en_US'
-        },
-        {
-          value: 'es_AR',
-          name: 'es_AR'
-        }
-      ]
-    },
-    {
-      id: 'country',
-      name: 'País',
-      values: [
-        {
-          value: 'AU',
-          name: 'Australia'
-        },
-        {
-          value: 'DE',
-          name: 'Alemanhã'
-        },
-        {
-          value: 'BR',
-          name: 'Brasil'
-        },
-        {
-          value: 'PT',
-          name: 'Portugal'
-        },
-        {
-          value: 'en_US',
-          name: 'EUA'
-        },
-        {
-          value: 'RU',
-          name: 'Rússia'
-        }
-      ]
-    },
-    {
-      id: 'timestamp',
-      name: 'Data e Horário',
-      validation: {
-        primitiveType: 'STRING',
-        entityType: 'DATE_TIME',
-        pattern: 'yyyy-MM-ddTHH:mm:ss'
-      }
-    },
-    {
-      id: 'limit',
-      name: 'Quantidade',
-      validation: {
-        primitiveType: 'INTEGER',
-        min: 1,
-        max: 50
-      }
-    },
-    {
-      id: 'offset',
-      name: 'Página',
-      validation: {
-        primitiveType: 'INTEGER'
-      }
-    }
-  ]
-}
-*/
 class FeaturedPlaylist extends React.Component {
   constructor(props) {
     super(props)
-
     const { dispatch } = props
-
     this.boundActionCreators = bindActionCreators(storeActions, dispatch)
   }
 
@@ -150,6 +57,7 @@ class FeaturedPlaylist extends React.Component {
     if (this.props.loading) {
       return <Loading />
     }
+
     return (
       <WarpFilter>
         <div className="coutries">
