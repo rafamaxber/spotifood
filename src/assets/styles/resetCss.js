dd41220021c8ee4ac240609fb14c28fb5a3deaf2
Rafael Bernardo <rafamaxber@gmail.com>
2018-10-23 03:01:16 -0300

    Update styles with styled components

---
+++ src/assets/styles/resetCss.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const cssVariables = {
+  $cssMainColor: "272727",
+  $cssSecundaryColor: "1ed760",
+  $cssSuccessColor: "00e900",
+  $cssSiteSize: "1340px",
+}
+
+const GlobalStyle = createGlobalStyle`
+
+  body {
+    --main-color: #272727;
+    --secundary-color: #1ed760;
+    --success-color: #00e900;
+    --site-size: 1340px;
+
+    background-color: var(--main-color);
+    font-family: "-apple-system","BlinkMacSystemFont","Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`
+export default GlobalStyle
