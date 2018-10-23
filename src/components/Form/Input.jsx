f49f74b5a490b60d2e839163455f47f7ecdaca74
Rafael Bernardo <rafamaxber@gmail.com>
2018-10-23 02:24:39 -0300

    Add Form component

---
+++ src/components/Form/Input.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  border: none;
+`
+
+export const BigInputTheme = styled(Input)`
+  width: 100%;
+  height: 80px;
+  margin: 0 auto 2%;
+  padding: 2%;
+  font-weight: bold;
+  font-size: 25px;
+  background-color: #171717;
+  color: #fff;
+  ::placeholder {
+    font-weight: normal;
+    color: #fff;
+    opacity: 0.2;
+  }
+`
+
+export default props => <Input {...props} />
