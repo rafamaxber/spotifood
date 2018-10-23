e9bc222cd64e0cc61848f0ec1bd0894a87bf2bfc
Rafael Bernardo <rafamaxber@gmail.com>
2018-10-23 02:22:00 -0300

    Add Filter component

---
+++ src/components/Filters/Shared.jsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components'
+
+export const WarpFilter = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  border-bottom: 2px solid #fff;
+  padding: 10px 0;
+  margin: 10px 0;
+  .coutries {
+    width: 15%;
+    min-width: 170px;
+  }
+  .locale {
+    width: 23%;
+    min-width: 260px;
+  }
+  .timestamp {
+    width: 21%;
+    min-width: 260px;
+  }
+  .limit {
+    width: 20%;
+    min-width: 200px;
+  }
+  .offset {
+    width: 10%;
+    min-width: 120px;
+  }
+`
+
+export const Label = styled.label`
+  padding: 0 5px;
+  display: block;
+`
+
+export const Image = styled.img`
+  display: block;
+  width: 100%;
+  cursor: pointer;
+  transition: transform ease 0.2s;
+  :hover {
+    transform: scale(1.05);
+  }
+  :active {
+    transform: scale(0.9);
+  }
+`
+
+export const TitleFilter = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  display: block;
+  width: 100%;
+  color: #fff;
+  margin-bottom: 1%;
+`
