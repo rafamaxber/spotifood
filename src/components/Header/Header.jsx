f7ed1435f73ea54f489200ebf7c165e11d096405
Rafael Bernardo <rafamaxber@gmail.com>
2018-10-23 02:29:22 -0300

    Add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import styled from 'styled-components'
+import t from 'prop-types'
+import logo from '../../assets/images/spotifood-logo.svg'
+import svgSprite from '../../assets/images/sprite.svg'
+
+import { Limit } from '../Layout'
+
+const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 15px 0;
+  background-color: #000;
+  border-width: 3px;
+  border-style: solid;
+  border-image: linear-gradient(to bottom, black, rgba(0, 0, 0, 0)) 1 100%;
+  margin-bottom: 2%;
+  position: relative;
+  &:after {
+    content: '';
+    position: absolute;
+    height: 3px;
+    width: ${props => props.load}%;
+    left: 0;
+    bottom: -5px;
+    background: #1ed760;
+    background: -moz-linear-gradient(
+      left,
+      #1ed760 0%,
+      #e31c2b 49%,
+      #1ed760 100%
+    );
+    background: -webkit-linear-gradient(
+      left,
+      #1ed760 0%,
+      #e31c2b 49%,
+      #1ed760 100%
+    );
+    background: linear-gradient(
+      to right,
+      #1ed760 0%,
+      #e31c2b 49%,
+      #1ed760 100%
+    );
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1ed760', endColorstr='#1ed760',GradientType=1 );
+  }
+`
+
+const Logo = styled.a`
+  display: block;
+  margin: auto;
+`
+
+const Image = styled.img`
+  display: block;
+  max-height: 30px;
+  @media (max-width: 768px) {
+    max-height: 25px;
+  }
+`
+
+const FlexLimit = styled(Limit)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const SvgIcon = styled.svg`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+`
+
+const HeaderComponent = ({ toogleFilter, toogleSearchBar, loadProgress }) => (
+  <Header load={loadProgress}>
+    <FlexLimit>
+      {toogleFilter && (
+        <SvgIcon onClick={toogleFilter}>
+          <use xlinkHref={`${svgSprite}#icon-filter`} />
+        </SvgIcon>
+      )}
+      <Logo href="/">
+        <Image src={logo} />
+      </Logo>
+      {toogleSearchBar && (
+        <SvgIcon onClick={toogleSearchBar}>
+          <use xlinkHref={`${svgSprite}#icon-search`} />
+        </SvgIcon>
+      )}
+    </FlexLimit>
+  </Header>
+)
+
+HeaderComponent.propTypes = {
+  /** Should be use to toogle filter bar */
+  toogleFilter: t.func,
+  /** Should be use to toogle search bar */
+  toogleSearchBar: t.func,
+  /** Should be use to ornament or like a load progress(Not implemented)  */
+  loadProgress: t.number
+}
+
+HeaderComponent.defaultProps = {
+  loadProgress: 100
+}
+
+export default HeaderComponent
